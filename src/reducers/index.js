a2eafece0a291d4cbbd5f721bd7356eb78bed1c1
romasimeiz <roman.yephimov@gmail.com>
2017-08-01 10:10:24 +0300

    my books init commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import BookReducer from "./reducer_books";
+import ActiveBookReducer from "./reducer_active_book";
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  //state: (state = {}) => state
+  books : BookReducer,
+  activeBook : ActiveBookReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+	return [
+		{title : 'JavaSript Learn', pages: 234},
+		{title : 'Harry Potter', pages: 123},
+		{title : 'The Dark Tower', pages: 56},
+		{title : 'Eloquent Ruby', pages: 78},
+	]
+}
